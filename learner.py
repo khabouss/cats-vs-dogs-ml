3509fcbef36f4b720bfe07873e90387d21d8a05c
root <tkhabouss@leyton.com>
2023-12-14 16:49:56 +0100

    upload learner

---
+++ learner.py
@@ -0,0 +1,16 @@
+from duckduckgo_search import ddg_images
+from fastai.vision.all import *
+
+path = Path('training_set')
+
+dls = DataBlock(
+    blocks=(ImageBlock, CategoryBlock), 
+    get_items=get_image_files, 
+    splitter=RandomSplitter(valid_pct=0.2, seed=42),
+    get_y=parent_label,
+    item_tfms=[Resize(192, method='squish')]
+).dataloaders(path, bs=32)
+
+# print(dls.show_batch(max_n=6))
+learn = vision_learner(dls, resnet18, metrics=error_rate)
+learn.fine_tune(5)
